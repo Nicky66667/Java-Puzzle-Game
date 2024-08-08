9dab1f95db6ba40bd0c0dd1a38ff67038d90b4ab
nicky:D <148539721+Nicky66667@users.noreply.github.com>
2024-08-08 22:30:00 +0100

    removed the unused function

--- src/game/App.java
+++ src/game/App.java
@@ -1,10 +1,8 @@
 package game;
 
+// This is the entry of the app
 public class App {
     public static void main(String[] args) {
-            // 表示程序的启动入口
-
-            // 如果我们想要开启一个界面，就创建谁的对象就好了
 
            new GameFrame();
     }
--- src/game/GameFrame.java
+++ src/game/GameFrame.java
@@ -10,37 +10,35 @@ import java.nio.file.Path;
 import java.util.Objects;
 import java.util.Random;
 
+//This is the main frame of the game, included game logics.
 public class GameFrame extends JFrame implements KeyListener, ActionListener {
-    // JFrame表示界面，窗体
-    // 他的子类也是界面和窗体
-    // 规定: GameJFrame就是游戏的主页面
-    // 以后和游戏有关的所有逻辑都写在这个类中
 
+    //the size of puzzle is 4*4
     int[][] data= new int[4][4];
 
-    // 记录空白方块在数组中的位置
+    // records the location of the blank space in the array
     int x=0;
     int y=0;
 
-    // 定义一个变量 记录当前展示图片的路径
+    //the path to puzzle image folder
     String path = "image\\Cat\\cat2\\";
 
-    // 定义一个二维数组，存储正确的数据
+    //stores the correct order of image fragments定义一个二维数组，存储正确的数据
     int[][] win={
             {1,2,3,4},
             {5,6,7,8},
             {9,10,11,12},
             {13,14,15,0}
     };
-    // 定义变量统计步数
+    // records the steps of movement
     int step =0;
 
-    // 创建下面的条目对象
+    //The menu items of function
     JMenuItem replayItem = new JMenuItem("restart game");
     JMenuItem closeItem = new JMenuItem("close game");
     JMenuItem accountItem = new JMenuItem("My selfie");
 
-    // 创建图片选项
+    //The image options
     JMenuItem TFItem = new JMenuItem("TFboys");
     JMenuItem CatItem = new JMenuItem("Cat");
     JMenuItem ikunItem = new JMenuItem("ikun");
@@ -48,139 +46,148 @@ public class GameFrame extends JFrame implements KeyListener, ActionListener {
     public GameFrame(){
 
 
-        // 初始化界面
+        //initial game frame
         initJFrame();
 
-        // 初始化菜单
+        //initial menu items
         initJMenuBar();
 
-        // 初始化数据(打乱)
+        //initial data(shuffle the fragments)
         initData();
 
-        // 初始化图片
+        //initial image
         initImage();
 
-        // 显示用户指南
+        //initial puzzle instruction
         initialInstruction();
 
 
-        // 显示写在最后
+        //set the puzzle visible
         this.setVisible(true);
     }
 
+    //Initializes and displays an instructional dialog.
     private void initialInstruction(){
+        //create a dialog window
         JDialog jD = new JDialog();
-        // 创建一个管理图片的容器对象
+        //contains a JLabel with an image of instruction
         JLabel jLabel = new JLabel(new ImageIcon("sporter\\instruction.png"));
         jLabel.setBounds(0,0,584,312);
-        // 把图片添加到弹框之中
+
         jD.getContentPane().add(jLabel);
-        // 给弹窗设置大小
+        // set the size of dialog
         jD.setSize(600,350);
-        // 让弹框置顶
+        // set the dialog on the top
         jD.setAlwaysOnTop(true);
-        // 让弹框显示出来
+        // display dialog
         jD.setVisible(true);
 
     }
 
 
 
-    // 打乱数据
+    //Initializes and shuffles the data array.
     private void initData() {
         int[] tempArr = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
 
-        // 遍历数组，得到每一个元素，拿着每一个元素跟随机索引上的数据进行交换
+        //Shuffle the array by swapping each element with a randomly chosen element
         Random r = new Random();
         for (int i = 0;i<tempArr.length;i++){
+            //Generate a random index
             int index = r.nextInt(tempArr.length);
+
+            //Swap the current element with the element at the random index.
             int temp = tempArr[i];
             tempArr[i] = tempArr[index];
             tempArr[index] = temp;
 
         }
 
+        //Populate a 4x4 matrix with the shuffled numbers.
         for (int i = 0;i<tempArr.length;i++){
             if (tempArr[i]==0){
                 x=i/4;
                 y=i%4;
             }
+
+            //Assign the shuffled number to the appropriate position in the 4x4 matrix.
             data[i / 4][i % 4] = tempArr[i];
         }
 
     }
 
-    // 初始化图片
-    // 按照二维数组中管理的数据添加图片
+    // Initializes and updates the UI components for the game window.
     private void initImage() {
-        // 清空原本已经出现的所有图片
+
+        //Remove all existing components from the content pane.
         this.getContentPane().removeAll();
 
+        //Check if the victory condition is met.
         if (victory()){
-            // 显示胜利图标
+
+            // Create and add a victory image label to the content pane.
             JLabel winJLabel = new JLabel(new ImageIcon("sporter\\win.jpg"));
             winJLabel.setBounds(190,283,288,175);
             this.getContentPane().add(winJLabel);
         }
 
+        // Create and add a label to display the step counter.
         JLabel stepCount = new JLabel("steps counter: "+step);
         stepCount.setBounds(50,30,100,20);
         this.getContentPane().add(stepCount);
-        // 绝对路径: 一定从盘符开始  e.g. C:\\
-        // 相对路径: 不是从盘符开始: aaa\\ bbb, 指的是在当前项目下，去找aaa文件夹，再找bbb
 
-        // 外循环---把内循环重复执行四次
+        // Iterate over the grid to populate image tiles.
         for (int i=0;i<4;i++){
-            // 内循环---表示在一行添加4张图片
-            for (int j=0;j<4;j++){
-                // 获取当前要加载图片的序号
-                int num = data[i][j];
-                // 创建一个JLabel的对象(管理容器)
 
+            for (int j=0;j<4;j++){
+
+                // Retrieve the number for the current tile from the data array.
+                int num = data[i][j];
+
+                // Create a label with an image icon based on the tile number.
                 JLabel jLabel = new JLabel(new ImageIcon(path+num+".jpg"));
 
-                // 给图片添加边框
+                // Set a border for the tile image.
                 jLabel.setBorder(new BevelBorder(1));
-                // 指定图片位置
+
+                // Set the position and size of the tile image.
                 jLabel.setBounds(105*j+83,105*i+134,105,105);
 
-                // 把管理容器添加到界面
-                //this.add(jLabel);
+                // Set the position and size of the tile image.
                 this.getContentPane().add(jLabel);
 
-                // 刷新一下界面
+                // Repaint the content pane to reflect the changes.
                 this.getContentPane().repaint();
             }
         }
-        // 添加背景图片
+
+        // Create and add a background image label to the content pane.
         ImageIcon bg = new ImageIcon("sporter\\background.jpg");
         JLabel background = new JLabel(bg);
         background.setBounds(40,40,508,560);
-        // 把背景图片添加到界面中
+
+        // Add the background image to the content pane.
         this.getContentPane().add(background);
 
     }
 
 
+    // Initializes and sets up the menu bar
     private void initJMenuBar() {
-        // 创建整个菜单对象
+        // Create a new JMenuBar instance to hold the menus.
         JMenuBar jMenuBar = new JMenuBar();
 
-        // 创建菜单式的两个选项的对象(功能，关于我们)
+        // Create the main menu items: "Function"
         JMenu functionJMenu = new JMenu("Function");
-        JMenu aboutJMenu = new JMenu("About developer");
 
-        // 创建更改图片的选项功能
+        // Create a submenu under "Function" for changing pictures.
         JMenu changeMenu = new JMenu("change Picture");
 
-
-        // 将每一个选项下面的条目添加到选项中
+        // Add menu items to the "Function" menu.
         functionJMenu.add(replayItem);
         functionJMenu.add(closeItem);
 
-        aboutJMenu.add(accountItem);
-
-        // 给条目绑定事件
+        // Attach action listeners to menu items to handle user actions.
         replayItem.addActionListener(this);
         closeItem.addActionListener(this);
         accountItem.addActionListener(this);
@@ -188,36 +195,37 @@ public class GameFrame extends JFrame implements KeyListener, ActionListener {
         CatItem.addActionListener(this);
         ikunItem.addActionListener(this);
 
-        // 将菜单里面的两个选项添加到菜单当中
+        // Add the "Function" menus to the menu bar.
         jMenuBar.add(functionJMenu);
-        jMenuBar.add(aboutJMenu);
 
-        // 把图片选项添加到更改图片菜单中
+        // Add items to the "change Picture" submenu under "Function".
         changeMenu.add(TFItem);
         changeMenu.add(CatItem);
         changeMenu.add(ikunItem);
 
+        // Add the "change Picture" submenu to the "Function" menu.
         functionJMenu.add(changeMenu);
 
-
-        // 给整个界面设置菜单
+        // Set the menu bar for the current window.
         this.setJMenuBar(jMenuBar);
     }
 
+    //Initializes the main JFrame settings
     private void initJFrame() {
+        // Set the size of the JFrame
         this.setSize(603,680);
-        // 设置界面的标题
+        // Set the title of the JFrame window
         this.setTitle("Puzzle-offline v1.0");
-        // 设置界面置顶
+        // Make the JFrame always appear on top of other windows.
         this.setAlwaysOnTop(true);
-        // 设置界面居中
+        // Center the JFrame on the screen.
         this.setLocationRelativeTo(null);
-        // 设置关闭模式
+        // Set the default close operation to exit.
+        // The parameter value 3 corresponds to JFrame.EXIT_ON_CLOSE.
         this.setDefaultCloseOperation(3);
-
-        // 取消默认的居中位置，只有取消了才会按照XY轴的形式添加图片
+        // Set the layout manager of the JFrame to null, allowing manual positioning of components.
         this.setLayout(null);
-        // 给整个界面添加键盘监听事件
+        // Add a key listener to the JFrame to handle key events.
         this.addKeyListener(this);
     }
 
@@ -226,101 +234,112 @@ public class GameFrame extends JFrame implements KeyListener, ActionListener {
 
     }
 
-    // 按下不松调用这个方法
+    // Handles the event when a key is pressed
     @Override
     public void keyPressed(KeyEvent e){
+        // Retrieve the key code from the KeyEvent object.
         int code = e.getKeyCode();
+        // Check if the key code corresponds to the 'A' key
         if (code==65){
-            // 把界面里所有的图片删除
+            // Remove all components from the content pane
             this.getContentPane().removeAll();
-            // 加载第一张完整图片
+            // display the current puzzle completed image
             JLabel all= new JLabel(new ImageIcon(path+"all.jpg"));
             all.setBounds(83,134,420,420);
             this.getContentPane().add(all);
-            // 加载背景图片
+            // Create an ImageIcon for the background image
             ImageIcon bg = new ImageIcon("sporter\\background.jpg");
             JLabel background = new JLabel(bg);
             background.setBounds(40,40,508,560);
-            // 把背景图片添加到界面中
+            // Create an ImageIcon for the background image
             this.getContentPane().add(background);
-            // 刷新界面
+            // Repaint the content pane to reflect the changes
             this.getContentPane().repaint();
         }
 
     }
 
+    //Handles the event when a key is released
     @Override
     public void keyReleased(KeyEvent e) {
-        // 判断游戏是否胜利，如果胜利，此方法结束，不能再执行下面的移动代码
+        // Check if the game is in a victory state
         if (victory()){
-            // 1返回值 2结束方法
             return;
         }
-        // 对上下左右进行判断
-        // 左: 37 上:38 右:39 下:40
+        // Retrieve the key code
         int code =e.getKeyCode();
+
+        // Handle the left arrow key
         if (code==37){
-            System.out.println("向左移动");
+            System.out.println("move left");
             if (y==3){
                 return;
             }
-            // 逻辑: 把空白块左方的数字往右移动
+            //shift the value from the right to the current position.
             data[x][y] = data[x][y+1];
             data[x][y+1]=0;
+            // Move the position to the left.
             y++;
-            // 每移动一次，计数器自增一次
+            // Increment the step count.
             step++;
-            // 调用方法按照最新的数字加载图片
+            // Refresh the display to reflect changes.
             initImage();
+
+            //Handle the up arrow key
         } else if (code==38) {
-            // 每移动一次，计数器自增一次
+            // Increment the step count.
             step++;
-            System.out.println("向上移动");
+            System.out.println("move up");
+            // Check if the x position is at the top edge
             if (x==3){
-                // 说明空白方块已经在最下方了，下面没有图片可以移动啦
                 return;
             }
-            // 逻辑: 把空白块下方的数字往上移动
-            // x，y表示空白方块
-            // 把空白方块下方的数字赋值给空白方块
+            //shift the value from below to the above position.
             data[x][y] = data[x+1][y];
             data[x+1][y]=0;
             x++;
-            // 调用方法按照最新的数字加载图片
+
             initImage();
 
+            // Handle the right arrow key
         } else if (code==39) {
-            // 每移动一次，计数器自增一次
+
             step++;
-            System.out.println("向右移动");
+            System.out.println("move right");
             if (y==0){
                 return;
             }
-            // 逻辑: 把空白块左方的数字往右移动
+            //shift the value from the left to the right position
             data[x][y] = data[x][y-1];
             data[x][y-1]=0;
             y--;
-            // 调用方法按照最新的数字加载图片
+
             initImage();
 
 
+            // Handle the down arrow key
         } else if (code==40) {
-            // 每移动一次，计数器自增一次
+
             step++;
-            System.out.println("向下移动");
+            System.out.println("move down");
             if (x==0){
                 return;
             }
-            // 逻辑: 把空白块上方的数字往右移动
+            //shift the value from above to the below position.
             data[x][y] = data[x-1][y];
             data[x-1][y]=0;
             x--;
-            // 调用方法按照最新的数字加载图片
+
             initImage();
 
+            // Handle the 'A' key
         } else if (code==65) {
             initImage();
+
+            // Handle the 'W' key
         } else if (code==87) {
+
+            // Reset the data array to its initial state.
             data=new int[][]{
                     {1,2,3,4},
                     {5,6,7,8},
@@ -333,12 +352,14 @@ public class GameFrame extends JFrame implements KeyListener, ActionListener {
 
     }
 
-    // 判断data数组的数据是否和win相同
+    // check if the sort of image fragments matchs the correct sort
     public boolean victory(){
+        // Iterate through each row
         for (int i=0; i<data.length;i++){
+            // Iterate through each column
             for (int j=0; j<data[i].length;j++){
+                // Compare the current element with the corresponding element in the winning state array.
                 if (data[i][j] != win[i][j]){
-                    // 只要一个数据不一样就返回false
                     return false;
                 }
             }
@@ -346,45 +367,37 @@ public class GameFrame extends JFrame implements KeyListener, ActionListener {
         return true;
     }
 
+
+    // Handles action events triggered by menu item selections.
     @Override
     public void actionPerformed(ActionEvent e) {
-        // 获取当前被点击的条目对象
+        // Get the source of the action event.
         Object obj = e.getSource();
 
-        // 判断
+        // Handle the replay item action.
         if (obj==replayItem){
-            // 计步器归零
+            // Reset the step counter to zero.
             step=0;
 
-            // 再次打乱二维数组中的数据
+
             initData();
-            // 重新加载游戏
+
             initImage();
 
+            // Handle the close item action.
         } else if (obj==closeItem) {
-            // 直接关闭虚拟机
+            // Exit the application.
             System.exit(0);
-        }else if(obj==accountItem){
-            // 创建一个弹窗对象
-            JDialog jDialog = new JDialog();
-            // 创建一个管理图片的容器对象
-            JLabel jLabel = new JLabel(new ImageIcon("sporter\\information1.jpg"));
-            jLabel.setBounds(0,0,420,420);
-            // 把图片添加到弹框之中
-            jDialog.getContentPane().add(jLabel);
-            // 给弹窗设置大小
-            jDialog.setSize(400,400);
-            // 让弹框置顶
-            jDialog.setAlwaysOnTop(true);
-            // 让弹框居中
-            jDialog.setLocationRelativeTo(null);
-            // 弹框不关闭就无法操作下面的界面
-            jDialog.setModal(true);
-            // 让弹框显示出来
-            jDialog.setVisible(true);
+
+            // Handle the account item action.
+
+            // Handle the TFItem image
         } else if (obj==TFItem) {
+            // Handle the TFItem action.
             Random r = new Random();
             int num=r.nextInt(4)+1;
+
+            // Set the image path based on the random number.
             path="image\\TFboys\\tf"+num+"\\";
             initImage();
         } else if (obj==CatItem) {
